d0ce5dd7eb8d6f285d44dabe4b814eb8713c303a
Kurios Creative Design <glenn@kurioscreative.com>
2011-04-07 11:53:20 -0400

    Fixed is_featured to featured

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ splash = Page.create(:body => "Splash page.", :title => "Splash Title", :page_na
 
 post = Post.create(:title => "First Post", :summary => "Lorem ipsum", :body => "Lorem ipsum est dolorum.", :author => "The Birdsuit")
 
-featured_post = Post.create(:title => "Featured Post", :summary => "Lorem ipsum", :body => "Lorem ipsum est dolorum.", :author => "The Birdsuit", :is_featured => true, :featured_img_url => "http://placehold.it/590x300")
+featured_post = Post.create(:title => "Featured Post", :summary => "Lorem ipsum", :body => "Lorem ipsum est dolorum.", :author => "The Birdsuit", :featured => true, :featured_img_url => "http://placehold.it/590x300")
 
 artist = Artist.create(:name => "Matty Mayz", :bio_pic_url => "http://placehold.it/220x280", :bio_pic_hover_url => "http://placehold.it/220x280", :biography_text => "Lorem ipsum", :twitter_id => "MattyMayz", :roster_pic_url => "http://placehold.it/270x100", :roster_pic_hover_url => "http://placehold.it/270x100", :bio_video_img_url => "http://placehold.it/270x16", :bio_video_img_hover_url => "http://placehold.it/270x16")
 
