5dea43be58d5cd65c40e982ff9dae3b9405c6a64
Kurios Creative Design <glenn@kurioscreative.com>
2011-04-07 12:07:58 -0400

    Added published_date to seeds to meet validity test

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,13 @@ contact = Page.create(:body => "Lorem ipsum est.", :title=> "Contact Information
 
 splash = Page.create(:body => "Splash page.", :title => "Splash Title", :page_name => "splash", :page_img_url => "http://placehold.it/910400", :activate_as_splash => false)
 
-post = Post.create(:title => "First Post", :summary => "Lorem ipsum", :body => "Lorem ipsum est dolorum.", :author => "The Birdsuit")
+post = Post.create(:title => "First Post", :summary => "Lorem ipsum", :body => "Lorem ipsum est dolorum.", :author => "The Birdsuit", :published_date => DateTime.now)
 
-featured_post = Post.create(:title => "Featured Post", :summary => "Lorem ipsum", :body => "Lorem ipsum est dolorum.", :author => "The Birdsuit", :featured => true, :featured_img_url => "http://placehold.it/590x300")
+featured_post = Post.create(:title => "Featured Post", :summary => "Lorem ipsum", :body => "Lorem ipsum est dolorum.", :author => "The Birdsuit", :featured => true, :featured_img_url => "http://placehold.it/590x300", :published_date => DateTime.now)
 
 artist = Artist.create(:name => "Matty Mayz", :bio_pic_url => "http://placehold.it/220x280", :bio_pic_hover_url => "http://placehold.it/220x280", :biography_text => "Lorem ipsum", :twitter_id => "MattyMayz", :roster_pic_url => "http://placehold.it/270x100", :roster_pic_hover_url => "http://placehold.it/270x100", :bio_video_img_url => "http://placehold.it/270x16", :bio_video_img_hover_url => "http://placehold.it/270x16")
 
 release = Release.create(:artist_id => artist.id, :release_img_url => "http://placehold.it/270x160", :release_img_hover_url => "http://placehold.it/270x160", :video_url => "http://vimeo.com/21919856", :title => "Test Release", :description => "Test video", :is_featured => false)
 
 featured_release = Release.create(:artist_id => artist.id, :release_img_url => "http://placehold.it/270x160", :release_img_hover_url => "http://placehold.it/270x160", :video_url => "http://vimeo.com/21919856", :title => "Test Featured", :description => "Test video", :is_featured => true)
+
